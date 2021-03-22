c1f8082f92d12be53cc4bc3f5df00d2276fcda7e
Jan Starke <Jan.Starke@t-systems.com>
2021-03-22 16:38:48 +0100

    added implementation for only_delimited

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 struct CutOptions {
     input_delimiter: String,
     output_delimiter: String,
-    only_delimited: bool,
+    notonly_delimited: bool,
     complement: bool,
     fields: Vec<String>,
     wrap_lines: String,
@@ -18,7 +18,7 @@ impl CutOptions {
         CutOptions {
             input_delimiter: String::new(),
             output_delimiter: String::from(" & "),
-            only_delimited: false,
+            notonly_delimited: false,
             complement: false,
             fields: Vec::new(),
             wrap_lines: String::new(),
@@ -27,6 +27,14 @@ impl CutOptions {
     }
 
     fn cut_line(&self, line: &str) -> std::io::Result<()> {
+        if ! line.contains(&self.input_delimiter) {
+            if ! self.notonly_delimited {
+                println!("{}", line);
+            }
+            return Ok(())
+        }
+
+        println!("{}", line);
         Ok(())
     }
 
@@ -46,7 +54,7 @@ fn main() -> Result<(), String> {
         ap.set_description("formats a table-like stream into a LaTeX-table");
         ap.refer(&mut options.input_delimiter).add_option(&["-d", "--delimiter"], Store, "use this character for field delimiter").required();
         ap.refer(&mut options.fields).add_option(&["-f"], List, "select only these fields;  also print any line that contains no delimiter character, unless the -s option is specified").required();
-        ap.refer(&mut options.only_delimited).add_option(&["-s", "--only-delimited"], StoreTrue, "do not print lines not containing delimiters");
+        ap.refer(&mut options.notonly_delimited).add_option(&["-s", "--only-delimited"], StoreTrue, "do not print lines not containing delimiters");
         ap.refer(&mut options.complement).add_option(&["--complement"], StoreTrue, "complement the set of selected bytes, characters or fields");
         ap.refer(&mut options.wrap_lines).add_option(&["-l", "--wrap-lines"], Store, "use this LaTeX command to wrap lines");
         ap.refer(&mut options.wrap_fields).add_option(&["-c", "--wrap-columns"], List, "use this LaTeX commands to wrap columns");
